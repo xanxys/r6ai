bd240daa194c2ef0e853a405887a288acab246e1
xyx <xanxys@gmail.com>
2022-12-10 04:16:24 +0900

    Implement AlphaZero algo

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@ mod r6;
 
 use std::{
     collections::HashMap,
+    fs::File,
     hash::Hash,
-    time::{Instant, SystemTime, UNIX_EPOCH}, fs::File,
+    time::{Instant, SystemTime, UNIX_EPOCH},
 };
 
 use crate::r6::Board;
-use r6::{Action, Cell};
+use dfdx::{
+    prelude::{HasArrayData, Linear, Module, ReLU, ResetParams, Sigmoid, SplitInto},
+    tensor::{Tensor1D, TensorCreator},
+};
+use r6::{to_ix, Action, Cell, PutAction, B_SIZE};
 use rand::{rngs::SmallRng, Rng, SeedableRng};
 
 fn random_get_action(rng: &mut SmallRng, b: &Board, budget_usec: u64) -> Action {
@@ -167,11 +172,240 @@ fn mcts_get_action(rng: &mut SmallRng, b: &Board, budget_usec: u64) -> Action {
     a
 }
 
+const STATE_SIZE: usize = 3 * 36 + 2; // cell (E, B, W) x 6^2 + curr_side (B, W)
+const ACTION_SIZE: usize = 36 + 1; // put & pass
+
+type PVNetwork = (
+    (Linear<STATE_SIZE, 32>, ReLU),
+    (Linear<32, 32>, ReLU),
+    // selector: logits for action (put: 36, pass: 1)
+    // value: 0~1 expected final value
+    SplitInto<(Linear<32, ACTION_SIZE>, (Linear<32, 1>, Sigmoid))>,
+);
+
+fn encode_board(b: &Board) -> Tensor1D<STATE_SIZE> {
+    let mut s: Tensor1D<STATE_SIZE> = TensorCreator::zeros();
+    let mut sd = s.mut_data();
+
+    // one-hot encoding of cells
+    for y in 0..B_SIZE {
+        for x in 0..B_SIZE {
+            let v_ofs = 3 * to_ix(x, y);
+            match b.cells[to_ix(x, y)] {
+                Cell::Empty => {
+                    sd[v_ofs + 0] = 1.0;
+                }
+                Cell::Black => {
+                    sd[v_ofs + 1] = 1.0;
+                }
+                Cell::White => {
+                    sd[v_ofs + 2] = 1.0;
+                }
+            }
+        }
+    }
+
+    // one-hot encoding of side
+    if b.side_black {
+        sd[3 * 36 + 0] = 1.0;
+    } else {
+        sd[3 * 36 + 1] = 1.0;
+    }
+
+    s
+}
+
+fn decode_legal_action(b: &Board, p: &Tensor1D<ACTION_SIZE>) -> Vec<(Action, f32)> {
+    let probs = p.clone().log_softmax();
+    let mut actions = Vec::with_capacity(ACTION_SIZE);
+    for y in 0..B_SIZE {
+        for x in 0..B_SIZE {
+            actions.push((
+                Action::Put(PutAction {
+                    x: x,
+                    y: y,
+                    c: if b.side_black {
+                        Cell::Black
+                    } else {
+                        Cell::White
+                    },
+                }),
+                probs.data()[to_ix(x, y)],
+            ));
+        }
+    }
+    actions.push((Action::Pass, probs.data()[36]));
+    actions
+}
+
+fn az_get_action(rng: &mut SmallRng, b: &Board, budget_usec: u64, pv: &PVNetwork) -> Action {
+    firestorm::profile_fn!(az_get_action);
+
+    let t0 = Instant::now();
+
+    const W_NET: f32 = 0.3;
+    const MIN_OBS_TO_EXPAND: i32 = 10;
+
+    struct MCTSNode {
+        parent: Option<Board>,
+        children: Vec<(Action, Board)>,
+        num_obs: i32,
+        tot_b_reward: f32,
+        net_prob: f32,
+    }
+    let mut dag: HashMap<Board, MCTSNode> = HashMap::new();
+    fn record_reward(dag: &mut HashMap<Board, MCTSNode>, b: &Board, r_b: f32) {
+        firestorm::profile_fn!(record_reward);
+
+        let mut b = b.clone();
+        loop {
+            match dag.get_mut(&b) {
+                Some(node) => {
+                    node.num_obs += 1;
+                    node.tot_b_reward += r_b;
+                    match &node.parent {
+                        Some(p) => b = p.clone(),
+                        None => break,
+                    }
+                }
+                None => {
+                    panic!("Tried to record reward for non-existent node");
+                }
+            }
+        }
+    }
+    fn insert_empty(dag: &mut HashMap<Board, MCTSNode>, b: &Board, parent: Option<Board>) {
+        firestorm::profile_fn!(insert_empty);
+
+        let node = MCTSNode {
+            parent: parent.clone(),
+            children: vec![],
+            num_obs: 0,
+            tot_b_reward: 0.0,
+            net_prob: -1.0,
+        };
+        if dag.insert(b.clone(), node).is_some() {
+            // this can very rarely happen when self-loop of BoardState exists, because of Pass action.
+            // TODO: is ignoring OK?
+        }
+    }
+    fn expand(rng: &mut SmallRng, dag: &mut HashMap<Board, MCTSNode>, b: &Board, pv: &PVNetwork) {
+        firestorm::profile_fn!(expand);
+
+        // Evaluate PV network.
+        let in_s = encode_board(b);
+        let (out_p, _) = pv.forward(in_s);
+        let actions = decode_legal_action(b, &out_p);
+
+        let legal_actions = b.legal_actions();
+        let p_sum_legal: f32 = actions
+            .iter()
+            .filter(|(a, _)| legal_actions.contains(a))
+            .map(|(_, p)| p)
+            .sum();
+
+        for (a, a_prob) in actions {
+            if !legal_actions.contains(&a) {
+                continue;
+            }
+
+            let mut b2 = b.clone();
+            b2.apply(&a);
+
+            insert_empty(dag, &b2, Some(b.clone()));
+            dag.get_mut(&b2).unwrap().net_prob = a_prob / p_sum_legal;
+            dag.get_mut(b).unwrap().children.push((a, b2));
+        }
+    }
+    fn selection_score(num_obs: i32, tot_b_reward: f32, net_prob: f32, num_obs_parent: i32) -> f32 {
+        let e_b_reward = if num_obs == 0 {
+            0.5
+        } else {
+            tot_b_reward / num_obs as f32
+        };
+        e_b_reward + W_NET * net_prob * (num_obs_parent as f32).sqrt() / (1.0 + num_obs as f32)
+    }
+    fn pick_best(dag: &HashMap<Board, MCTSNode>, b: &Board) -> (Action, Board) {
+        firestorm::profile_fn!(pick_best);
+
+        let num_obs = dag.get(b).unwrap().num_obs;
+        dag.get(b)
+            .unwrap()
+            .children
+            .iter()
+            .max_by(|(_, b1), (_, b2)| {
+                let n1 = dag.get(&b1).unwrap();
+                let n2 = dag.get(&b2).unwrap();
+                let s1 = selection_score(n1.num_obs, n1.tot_b_reward, n1.net_prob, num_obs);
+                let s2 = selection_score(n2.num_obs, n2.tot_b_reward, n2.net_prob, num_obs);
+
+                if b.side_black {
+                    s1.partial_cmp(&s2).unwrap() // black's turn -> maximize black reward
+                } else {
+                    s2.partial_cmp(&s1).unwrap() // white's turn -> minimize black reward
+                }
+            })
+            .unwrap()
+            .clone()
+    }
+    fn select(
+        rng: &mut SmallRng,
+        dag: &mut HashMap<Board, MCTSNode>,
+        b0: &Board,
+        pv: &PVNetwork,
+    ) -> Vec<Action> {
+        let mut node = dag.get_mut(b0).expect("Invalid MCTS scan");
+        if node.children.is_empty() {
+            // not yet expanded node
+            if node.num_obs < MIN_OBS_TO_EXPAND {
+                return vec![];
+            } else {
+                // ok to expand now
+                expand(rng, dag, b0, pv);
+                node = dag.get_mut(b0).unwrap();
+            }
+        }
+        if node.children.is_empty() {
+            panic!("MCTS #children must be >0");
+        }
+
+        let (a, b) = pick_best(dag, b0);
+        let mut ac_path = select(rng, dag, &b, pv);
+        ac_path.push(a);
+        return ac_path;
+    }
+
+    insert_empty(&mut dag, b, None);
+    expand(rng, &mut dag, b, pv);
+    let mut num_iter = 0;
+    loop {
+        if t0.elapsed().as_micros() as u64 >= budget_usec {
+            break;
+        }
+
+        let next_action = select(rng, &mut dag, b, pv).last().unwrap().clone();
+        let mut b2 = b.clone();
+        b2.apply(&next_action);
+        let (_, v) = pv.forward(encode_board(&b2));
+        record_reward(&mut dag, &b2, v.data()[0]);
+        num_iter += 1;
+    }
+
+    let (a, _) = pick_best(&dag, b);
+    println!(
+        "AZ: #nodes={} / #obs={} / #iter={}",
+        dag.len(),
+        dag.get(b).unwrap().num_obs,
+        num_iter
+    );
+    a
+}
+
 fn playout_b_reward(rng: &mut SmallRng, b: &Board) -> f32 {
     firestorm::profile_fn!(playout_b_reward);
 
     let mut curr_b = b.clone();
-    for i in 0..30 {
+    for i in 0..35 {
         if let Some(res) = curr_b.is_terminal() {
             match res {
                 Cell::Black => return 1.0,
@@ -186,13 +420,7 @@ fn playout_b_reward(rng: &mut SmallRng, b: &Board) -> f32 {
 
 // reward value is +1 for black win, -1 for white win, 0.5 for draw.
 
-fn win_rate<Fn1, Fn2>(
-    rng: &mut SmallRng,
-    p1: Fn1,
-    p2: Fn2,
-    num: usize,
-    budget_usec: u64,
-) -> f32
+fn win_rate<Fn1, Fn2>(rng: &mut SmallRng, p1: Fn1, p2: Fn2, num: usize, budget_usec: u64) -> f32
 where
     Fn1: Fn(&mut SmallRng, &Board, u64) -> Action,
     Fn2: Fn(&mut SmallRng, &Board, u64) -> Action,
@@ -214,14 +442,14 @@ where
                         } else {
                             num_win_p2 += 1;
                         }
-                    },
+                    }
                     Cell::White => {
                         if p1_is_black {
                             num_win_p2 += 1;
                         } else {
                             num_win_p1 += 1;
                         }
-                    },
+                    }
                 }
                 break;
             }
@@ -278,16 +506,17 @@ fn compare() {
             .as_micros() as u64,
     );
 
+    let mut pv = PVNetwork::default();
+    pv.reset_params(&mut rng);
+
     let mcts_1ms = |rng: &mut SmallRng, b: &Board, _| mcts_get_action(rng, b, 1000);
-    let mcts_10ms = |rng: &mut SmallRng, b: &Board, _| mcts_get_action(rng, b, 10000);
+    //let mcts_10ms = |rng: &mut SmallRng, b: &Board, _| mcts_get_action(rng, b, 10_000);
+    let az_1ms = |rng: &mut SmallRng, b: &Board, _| az_get_action(rng, b, 1000, &pv);
 
-    // mcts(10ms,release) v. mcts(1ms, release): 83%
-
-    win_rate(&mut rng, mcts_10ms, mcts_1ms, 1, 10000);
+    win_rate(&mut rng, az_1ms, mcts_1ms, 5, 10000);
 }
 
 fn main() {
-   
     //win_rate(&mut rng, random_get_action, mcts_get_action, 1, 10000);
     //play_single(&mut rng, mcts_get_action, random_get_action, 1000);
 
